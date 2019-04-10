acad2d136d5f882762fe64271b5d52b0ed3f8d54
Haco <877728156@qq.com>
2019-04-10 20:04:26 +0800

    add some extern dependent library

--- MathAlgorithm.java
+++ MathAlgorithm.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+//import java.util.Arrays;
+
 public class MathAlgorithm{
     //计算平方根（牛顿迭代法）
     public static double sqrt(double c){
@@ -40,18 +42,81 @@ public class MathAlgorithm{
             return mid;
     }
 
+    public static void RandomSeq (String[] args) {
+
+        // command-line arguments
+        int n = Integer.parseInt(args[0]);
+
+        // for backward compatibility with Intro to Programming in Java version of RandomSeq
+        if (args.length == 1) {
+            // generate and print n numbers between 0.0 and 1.0
+            for (int i = 0; i < n; i++) {
+                double x = StdRandom.uniform();
+                StdOut.println(x);
+            }
+        }
+
+        else if (args.length == 3) {
+            double lo = Double.parseDouble(args[1]);
+            double hi = Double.parseDouble(args[2]);
+
+            // generate and print n numbers between lo and hi
+            for (int i = 0; i < n; i++) {
+                double x = StdRandom.uniform(lo, hi);
+                StdOut.printf("%.2f\n", x);
+            }
+        }
+
+        else {
+            throw new IllegalArgumentException("Invalid number of arguments");
+        }
+    }
+
+    public static void Average(String[] args) { 
+        int count = 0;       // number input values
+        double sum = 0.0;    // sum of input values
+
+        // read data and compute statistics
+        while (!StdIn.isEmpty()) {
+            double value = StdIn.readDouble();
+            sum += value;
+            count++;
+        }
+
+        // compute the average
+        double average = sum / count;
+
+        // print results
+        StdOut.println("Average is " + average);
+    }
+
     public static void main(String[] args){
+        //白名单过滤
         /*
-        System.out.println(sqrt(2));
-        System.out.println(sqrt(3));
-        System.out.println(sqrt(5));
+        int[] whitelist=new In(args[0]).readAllInts();
+        Arrays.sort(whitelist);
+        while(!StdIn.isEmpty()){
+            int key=StdIn.readInt();
+            if(binarySearch(whitelist, key)<0)
+                StdOut.println(key);
+        }
         */
+
+        //模拟T次掷硬币
         /*
-        int a[]={1,4,7,9,6,3,2,5,8};
-        Arrays.sort(a);
-        System.out.println(binarySearch(a,9));
-        System.out.print(recursiveBS(9,a,0,a.length-1));
+        int T=Integer.parseInt(args[0]);
+        Counter heads=new Counter("heads");
+        Counter tails=new Counter("tails");
+        for(int t=0;t<T;t++)
+            if(StdRandom.bernoulli(0.5))
+                heads.increment();
+            else tails.increment();
+        StdOut.println(heads);
+        StdOut.println(tails);
+        int d=heads.tally()-tails.tally();
+        StdOut.println("delta: "+Math.abs(d));
         */
+
         
     }
 }
